2018881bfc709eac8079aab9568ac805ec0e913c
AeYunDian <zhanghaoyu19281@outlook.com>
2025-09-05 06:49:31 +0800

    Create golink.js

---
+++ database/work_wx/golink.js
@@ -0,0 +1,6 @@
+var link_data = 
+{
+"uid": 2001,
+"to": "https://work.weixin.qq.com/",
+"AR": [ "android_phone", "iPhone", "android_pad", "Windows_IE", "Windows_Chrome", "macOS", "iPad", "Symbian"]
+}
