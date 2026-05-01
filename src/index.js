4d9b1fbd5583731cada7ebaf3c3a726a1ebcea73
AeYunDian <zhanghaoyu19281@outlook.com>
2026-05-01 20:35:36 +0800

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -21,78 +21,102 @@ export default {
       'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type',
     };
-
-    // 处理预检请求
-    if (request.method === 'OPTIONS') {
-      return new Response(null, { headers: corsHeaders });
-    }
-
-    if (request.method === 'GET') {
-      let response = new Response('404 Not Found', { status: 404 });
-      if (path === '/go/parse') {
-        response = await parseLink(request, env);
+    try {
+      // 处理预检请求
+      if (request.method === 'OPTIONS') {
+        return new Response(null, { headers: corsHeaders });
       }
-      if (path === '/go/init') {
-        response = await initLink(request, env);
-      }
-      if (path === "/trigger") {
-        try {
-          await triggerWorkflow(env);
-          return new Response("Workflow triggered successfully", { status: 200 });
-        } catch (err) {
-          return new Response("Failed to trigger workflow: " + err.message, { status: 500 });
+
+      if (request.method === 'GET') {
+        let response = new Response("404 Not found",
+          {
+            status: 302,
+            headers: { 'Location': url.protocol + '//' + url.hostname, }
+          });
+
+        if (path === '/go/parse') {
+          response = await parseLink(request, env);
         }
-      }
+        if (path === '/go/init') {
+          response = await initLink(request, env);
+        }
+        if (path.startsWith('/gh/')) {
+          const gh_path = path.replace("/gh/", "");
+          try {
+            const gh_response = await fetch(gh_path, {
+              method: "GET",
+            });
+            if (!gh_response.ok) throw new Error(`Upstream returned ${gh_response.status}`);
+            response = new Response(
+              gh_response.body,
+              { headers: { 'Content-Type': gh_response.headers.get('Content-Type') || 'text/plain', } });
+
+          } catch (e) {
+            response = new Response("500 Server Error",{status: 500,});
+          }
+        }
+
+        if (path === "/trigger") {
+          try {
+            await triggerWorkflow(env);
+            return new Response("Workflow triggered successfully", { status: 200 });
+          } catch (err) {
+            return new Response("Failed to trigger workflow: " + err.message, { status: 500 });
+          }
+        }
 
 
-      for (const [key, value] of Object.entries(corsHeaders)) {
-        response.headers.set(key, value);
+        for (const [key, value] of Object.entries(corsHeaders)) {
+          response.headers.set(key, value);
+        }
+        return response;
       }
-      return response;
+
+      // 路由处理
+      if (request.method === 'POST') {
+        let response = new Response('404 Not Found', { status: 404 });
+        if (path === '/api/verifymail/v1/send') {
+          response = await handleSendVerification(request, env);
+        }
+        if (path === '/api/verifymail/v1/verify') {
+          response = await handleVerifyCode(request, env);
+        }
+        if (path === '/api/crossfire/v1/account/create') {
+          response = await CreateAccount(request, env);
+        }
+        if (path === '/api/crossfire/v1/account/init') {
+          response = await InitDatabase(request, env);
+        }
+        if (path === '/api/crossfire/v1/account/login') {
+          response = await Login(request, env);
+        }
+        if (path === '/api/crossfire/v1/account/logout') {
+          response = await Logout(request, env);
+        }
+        if (path === '/api/crossfire/v1/bag/get') {
+          response = await GetUserBag(request, env);
+        }
+        if (path === '/api/crossfire/v1/bag/push') {
+          response = await PushUserBag(request, env);
+        }
+        if (path === '/go/addlink') {
+          response = await addLink(request, env);
+        }
+
+        for (const [key, value] of Object.entries(corsHeaders)) {
+          response.headers.set(key, value);
+        }
+        return response;
+      }
+
+      return new Response('Not Found', {
+        status: 404,
+        headers: corsHeaders
+      });
+    } catch (err) {
+      console.error(err);
+      return new Response(`Worker threw exception: ${err.message}\nStack: ${err.stack || "no stack"}`, { status: 500, headers: { "Content-Type": "text/plain" } });
     }
-
-    // 路由处理
-    if (request.method === 'POST') {
-      let response = new Response('404 Not Found', { status: 404 });
-      if (path === '/api/verifymail/v1/send') {
-        response = await handleSendVerification(request, env);
-      }
-      if (path === '/api/verifymail/v1/verify') {
-        response = await handleVerifyCode(request, env);
-      }
-      if (path === '/api/crossfire/v1/account/create') {
-        response = await CreateAccount(request, env);
-      }
-      if (path === '/api/crossfire/v1/account/init') {
-        response = await InitDatabase(request, env);
-      }
-      if (path === '/api/crossfire/v1/account/login') {
-        response = await Login(request, env);
-      }
-      if (path === '/api/crossfire/v1/account/logout') {
-        response = await Logout(request, env);
-      }
-      if (path === '/api/crossfire/v1/bag/get') {
-        response = await GetUserBag(request, env);
-      }
-      if (path === '/api/crossfire/v1/bag/push') {
-        response = await PushUserBag(request, env);
-      }
-      if (path === '/go/addlink') {
-        response = await addLink(request, env);
-      }
-
-
-      for (const [key, value] of Object.entries(corsHeaders)) {
-        response.headers.set(key, value);
-      }
-      return response;
-    }
-
-    return new Response('Not Found', {
-      status: 404,
-      headers: corsHeaders
-    });
   }
 };
 
